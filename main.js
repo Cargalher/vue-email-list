0c1d629bce6fb7a661f781637789f1685e9c0882
Cargalher <cargalher@gmail.com>
2021-05-27 17:09:38 +0200

    exercise finished

--- main.js
+++ main.js
@@ -8,12 +8,15 @@ const app = new Vue ({
 
     },
     mounted (){
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        for (i=0; i<10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
         .then(response =>{
             // console.log(response.data.response);
-            this.emails = response.data.response;
+            this.emails.push(response.data.response);
             console.log(this.emails);
           
         })
+        }
+        
     }
 })
